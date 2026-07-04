63ace24a9e731cda21f5d27af5af9d727090ca00
Balajisaikrishna <velpulasaikrishnabalaji0608@gmail.com>
2026-07-04 15:22:09 +0530

    Learning from the solution

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const sidebar = document.querySelector('.left');
-const handle = document.querySelector('.resize-handle');
-let isResizing = false;
+// const sidebar = document.querySelector('.left');
+// const handle = document.querySelector('.resize-handle');
+// let isResizing = false;
 
-handle.addEventListener('mousedown',function(e){
-    isResizing= true;
-    document.body.style.cursor = 'col-resize';
-});
-document.addEventListener('mousemove',function(e){
-    if(!isResizing) return;
-    const newWidth = e.clientX;
-    if(newWidth > 200 && newWidth < 500){
-        sidebar.style.width = newWidth + 'px';
-    }
-});
-document.addEventListener('mouseup' ,function(){
-    isResizing = false;
-    document.body.style.cursor = 'default';
-});
+// handle.addEventListener('mousedown',function(e){
+//     isResizing= true;
+//     document.body.style.cursor = 'col-resize';
+// });
+// document.addEventListener('mousemove',function(e){
+//     if(!isResizing) return;
+//     const newWidth = e.clientX;
+//     if(newWidth > 200 && newWidth < 500){
+//         sidebar.style.width = newWidth + 'px';
+//     }
+// });
+// document.addEventListener('mouseup' ,function(){
+//     isResizing = false;
+//     document.body.style.cursor = 'default';
+// });
+//My js so far
